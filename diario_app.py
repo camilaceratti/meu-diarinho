89bf206138ed70f008c1facb4c1dd26c615df993
camilaceratti <caaceratti@gmail.com>
2026-04-04 20:19:54 -0300

    Update diario_app.py

--- diario_app.py
+++ diario_app.py
@@ -1,42 +1,64 @@
 import tkinter as tk
 from tkinter import messagebox
 import datetime
+import os
 
 def salvar():
     texto = entrada.get("1.0", tk.END).strip()
 
     if texto == "":
-        messagebox.showwarning("ops", "escreve alguma coisa 💖")
+        messagebox.showwarning("ops", "como você está se sentindo?")
         return
 
-    data = datetime.datetime.now().strftime("%d/%m/%Y")
+    pasta = "historico"
+    os.makedirs(pasta, exist_ok=True)
 
-    with open("diario.txt", "a", encoding="utf-8") as f:
-        f.write(f"{data} - {texto}\n")
+    data_arquivo = datetime.datetime.now().strftime("%d-%m-%Y")
+    caminho_arquivo = os.path.join(pasta, f"{data_arquivo}.txt")
+
+    with open(caminho_arquivo, "a", encoding="utf-8") as f:
+        f.write(texto + "\n")
 
     entrada.delete("1.0", tk.END)
-    messagebox.showinfo("💌", "salvo")
+    messagebox.showinfo("🌸 meu diarinho", "registrado\naté mais 💖")
 
-# janela
 janela = tk.Tk()
-janela.title("🌸 diarinho")
+janela.title("🌸 meu diarinho")
+janela.geometry("420x420")
+janela.configure(bg="#ffd6e7")
 
-# tamanho
-janela.geometry("400x400")
+titulo = tk.Label(
+    janela,
+    text="Bem-vinda ao seu diarinho 💖",
+    bg="#ffd6e7",
+    font=("Arial", 16)
+)
+titulo.pack(pady=15)
 
-# cor de fundo (rosinha)
-janela.configure(bg="#ffc0cb")
+subtitulo = tk.Label(
+    janela,
+    text="Como você está se sentindo?",
+    bg="#ffd6e7",
+    font=("Arial", 11)
+)
+subtitulo.pack(pady=5)
 
-# título
-titulo = tk.Label(janela, text="🌸 diarinho 💖", bg="#ffc0cb", font=("Arial", 16))
-titulo.pack(pady=10)
-
-# caixa de texto
-entrada = tk.Text(janela, height=10, width=35)
+entrada = tk.Text(
+    janela,
+    height=10,
+    width=35,
+    font=("Arial", 11)
+)
 entrada.pack(pady=10)
 
-# botão
-botao = tk.Button(janela, text="💌 salvar", command=salvar, bg="#ff69b4", fg="white")
+botao = tk.Button(
+    janela,
+    text="Salvar",
+    command=salvar,
+    bg="#ff8fb1",
+    fg="white",
+    font=("Arial", 11)
+)
 botao.pack(pady=10)
 
 janela.mainloop()
